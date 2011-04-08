b9bea3124319b67b1dd47ea7bc95b47e9a4d7e75
whatsinmyfridge <whatsinmyfridge@bf7e1a93-4ce7-49d1-8876-f7ef3d81b3a0>
2011-04-08 04:12:05 +0000

    recipe search by ingredients

--- web/recipe_search.php
+++ web/recipe_search.php
@@ -6,12 +6,30 @@
               . '?i=' . urlencode($ingredients)
 			  . '&onlyImages=1';
   $content = file_get_contents($queryUrl);
-  header('Content-type: application/json');
-  header($_SERVER["Server_PROTOCOL"] . " 200 OK");
+  //header('Content-type: application/json');
+  //header($_SERVER["Server_PROTOCOL"] . " 200 OK");
 
   if ($content) {
     $recipes = array();
     $obj = json_decode($content);
-    echo json_encode($obj->results);
+    //echo json_encode($obj->results);
+	$recipes = $obj->results;
   }
 ?>
+<div>
+	<div class="toolbar">
+		<h1>Recipe Results</h1>
+		<a href="#home" class="back">Back</a>
+	</div>
+	<div class='s-scrollwrapper'>
+		<div>
+			<ul class="rounded">
+			<?php
+				foreach($recipes as $recipe) {
+					echo '<li>' . $recipe->title . '</li>';
+				}
+			?>
+			</ul>
+		</div>
+	</div>
+</div>
