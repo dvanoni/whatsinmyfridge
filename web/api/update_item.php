5ef84b5462d2b6b39d98ec896193f615538c5801
whatsinmyfridge <whatsinmyfridge@bf7e1a93-4ce7-49d1-8876-f7ef3d81b3a0>
2011-04-08 15:18:45 +0000

    add item complete

--- web/api/update_item.php
+++ web/api/update_item.php
@@ -1,7 +1,22 @@
 <?php
-	$sensor = $_POST['id'];
-	$item   = $_POST['item'];
-	$name	= $_POST['name'];
+	$sensor = $_GET['id'];
+	$item   = $_GET['item'];
+	$name	= $_GET['name'];
+
+	$HOST = 'mysql.dvanoni.com';
+	$USER = 'whatsinmyfridge';
+	$PASS = 'coolfront';
+
+	// Connecting, selecting database
+	$link = mysql_connect( $HOST, $USER, $PASS ) or die('Could not connect: ' . mysql_error());
+	mysql_select_db('whatsinmyfridge') or die('Could not select database');
+
+	// Performing SQL query
+	$query = "UPDATE items SET icon='$item', name='$name' WHERE sensor=$sensor";
+	mysql_query($query) or die("adkfjadkfjka");
+	
+	// Closing connection
+	mysql_close($link);	
 	
 	$items = array( 'sensor' => $sensor, 'unknown' => false, 'name' => $name, 'weight' => 100.2, 'icon' => $item, 'age' => '5 days old' );
 	echo json_encode( $items );
