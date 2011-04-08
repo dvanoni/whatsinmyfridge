58f9846f88225525b647e46578790348299ecd0b
whatsinmyfridge <whatsinmyfridge@bf7e1a93-4ce7-49d1-8876-f7ef3d81b3a0>
2011-04-08 13:17:11 +0000

    stuff

--- web/api/check_fridge.php
+++ web/api/check_fridge.php
@@ -1,12 +1,12 @@
 <?php
 	$items = array();
-	$item = array( 'sensor' => 0, 'unknown' => false, 'name' => 'Apple ( Fuji )', 'weight' => 100.2, 'icon' => 'apple', 'age' => '5 days old' );
+	$item = array( 'sensor' => 0, 'unknown' => false, 'name' => 'Apple', 'weight' => 100.2, 'icon' => 'apple', 'age' => '5 days old' );
 	array_push( $items, $item );
-	$item = array( 'sensor' => 1, 'unknown' => false, 'name' => 'Pepper ( Fuji )', 'weight' => 19800.2, 'icon' => 'pepper', 'age' => '5 days old'  );
+	$item = array( 'sensor' => 1, 'unknown' => false, 'name' => 'Pepper', 'weight' => 19800.2, 'icon' => 'pepper', 'age' => '5 days old'  );
 	array_push( $items, $item );
-	$item = array( 'sensor' => 2, 'unknown' => false, 'name' => 'Muffin ( Fuji )', 'weight' => 109870.2, 'icon' => 'muffin', 'age' => '5 days old'  );
+	$item = array( 'sensor' => 2, 'unknown' => false, 'name' => 'Muffin', 'weight' => 109870.2, 'icon' => 'muffin', 'age' => '5 days old'  );
 	array_push( $items, $item );
-	$item = array( 'sensor' => 3, 'unknown' => true, 'name' => 'Apple ( Fuji )', 'weight' => 10870.2, 'icon' => 'apple', 'age' => '5 days old'  );
+	$item = array( 'sensor' => 3, 'unknown' => true, 'name' => 'Apple', 'weight' => 10870.2, 'icon' => 'apple', 'age' => '5 days old'  );
 	array_push( $items, $item );
 		
 	echo json_encode( $items );
