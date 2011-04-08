77f1bc6bbab3a9b6071dbd7ce5d13ec11fc4cc30
whatsinmyfridge <whatsinmyfridge@bf7e1a93-4ce7-49d1-8876-f7ef3d81b3a0>
2011-04-08 18:07:16 +0000

    adlfkjadkfjklalajdfladjfkljadkljf  adklfjakljaljlakj

--- web/api/get_friends.php
+++ web/api/get_friends.php
@@ -7,10 +7,13 @@
 	$item = array( 'id' => 2, 'name' => 'Andrew', 'food' => array( 'beef', 'milk', 'brie', 'wine' ) );
 	array_push( $items, $item );
 
-	$item = array( 'id' => 3, 'name' => 'Lynn', 'food' => array( 'muffin', 'milk', 'broccoli' ) );
+	$item = array( 'id' => 3, 'name' => 'Lynn', 'food' => array( 'muffin', 'milk', 'broccoli','wine' ) );
 	array_push( $items, $item );
 
-	$item = array( 'id' => 4, 'name' => 'George Clooney', 'food' => array( 'caviar', 'muffin', 'buffalo' ) );
+	$item = array( 'id' => 4, 'name' => 'Leilani', 'food' => array( 'muffin', 'spam', 'broccoli','wine' ) );
+	array_push( $items, $item );
+	
+	$item = array( 'id' => 5, 'name' => 'George Clooney', 'food' => array( 'caviar', 'muffin', 'buffalo' ) );
 	array_push( $items, $item );
 	
 	echo json_encode( $items );
