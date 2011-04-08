dccfeffeb307c53dd29e466d6961d02f44efa368
whatsinmyfridge <whatsinmyfridge@bf7e1a93-4ce7-49d1-8876-f7ef3d81b3a0>
2011-04-08 09:10:03 +0000

    better generic recipe search

--- web/recipe_search_list.php
+++ web/recipe_search_list.php
@@ -22,20 +22,10 @@
 	$recipes = $obj->results;
   }
 ?>
-<div>
-	<div class="toolbar">
-		<h1>Recipe Results</h1>
-		<a href="#home" class="back">Back</a>
-	</div>
-	<div class='s-scrollwrapper'>
-		<div>
-			<ul class="rounded">
-			<?php
-				foreach($recipes as $recipe) {
-					echo '<li>' . $recipe->title . '</li>';
-				}
-			?>
-			</ul>
-		</div>
-	</div>
-</div>
+<ul class="rounded">
+<?php
+	foreach($recipes as $recipe) {
+		echo '<li>' . $recipe->title . '</li>';
+	}
+?>
+</ul>
